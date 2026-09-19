9d1ce5dbd4c3753ecbd975e5b9ad487551183299
Theo Vale <theodoricovale@gmail.com>
2026-09-18 21:14:38 -0300

    Adicionando comentários pt1

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ LARGURA, ALTURA = 1500, 750
 JUMP_SPEED = 8
 INITIAL_X, INITIAL_Y = 750, 650
 LIMITE_QUEDA = ALTURA + 100
+MAX_JUMP = 30
+FORCA_MULT = 15
 
 pygame.init()
 tela = pygame.display.set_mode((LARGURA, ALTURA))
@@ -29,22 +31,46 @@ margem2 = Margem(LARGURA-200, 0)
 
 margem1.draw()
 margem2.draw()
-player = Sapo(INITIAL_X -30, INITIAL_Y - 25)
+
+
+player = Sapo(INITIAL_X, INITIAL_Y)
 player.pousar(plats[0])
 
 carregando_pulo = False
 jump_count = 0
-MAX_JUMP = 30
-FORCA_MULT = 15
+
 
 rodando = True
+
 while rodando:
+    # ==========================
+    # EVENTOS
+    # ==========================
     for evento in pygame.event.get():
+
+        # ==========================
+        # QUIT GAME
+        # ==========================
         if evento.type == pygame.QUIT:
             rodando = False
+
+        # ======================================================================================================================
+        # KEYDOWN
+        #   Identifica spacebar pressionado -> Ativa booleano de controle de pulo 'carregando_pulo' se player não estiver no ar 
+        # ======================================================================================================================
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
             if not player.pulando:     
                 carregando_pulo = True
+
+        # =======================================================================================
+        # KEYUP
+        #   Identifica spacebar liberado -> 
+        #       Desativa booleano de controle de pulo 'carregando_pulo'
+        #       Calcula distância a ser cobrida no pulo e atribui na variável 'força'
+        #       Chama método de movimento de player com parâmetro adequado 'player.pular(força)'
+        #       Reseta variável de acumulador de pulo 'jump_count' para futuras movimentações
+        # =======================================================================================
+        
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_SPACE:
             if carregando_pulo:
                 carregando_pulo = False
@@ -52,27 +78,51 @@ while rodando:
                 player.pular(forca)
                 jump_count = 0
 
+    
+    # ============================================================================================================
+    # RESET
+    #   Identifica tecla 0 pressionada -> Reseta posição do player para a primeira plataforma, propósito de testes
+    #   TODO: Mover método para dentro da classe Sapo
+    # ============================================================================================================
     if pygame.key.get_pressed()[pygame.K_0]:
-        player.x, player.y = 720, 625
+        player.x = INITIAL_X - Sapo.regulate_x
+        player.y = INITIAL_Y - Sapo.regulate_y
         player.no_ar = False
         player.vel_y = 0
 
-    tela.fill(AZUL_AGUA)
 
-    if carregando_pulo and jump_count < MAX_JUMP:
-        jump_count += 1
+    # ==========================
+    # DESENHOS
+    # ==========================
+    tela.fill(AZUL_AGUA) # Pinta tela inteira
 
-    plataforma_atual = player.check_underneath(plats)
-    
+
+    # Pecorrendo array de plataformas
     for plat in plats:
         if plat == plataforma_atual:
             plat.speed = 1.5
-       
-        plat.move_x_asis()
-        plat.desenhar_plat(tela)
+        plat.move_x_asis() # Movimento horizontal 
+        plat.desenhar_plat(tela) # Desenho da plataforma
 
+
+    # Exibição das margens 
     tela.blit(margem1.superficie, (margem1.x, margem1.y))
     tela.blit(margem2.superficie, (margem2.x, margem2.y))
+
+    # ==========================
+    # MOVIMENTAÇÃO
+    # ==========================
+
+    # -------------------------------------------------------------------------------------------------------------
+    # JUMP_COUNT
+    #   carregando_pulo: Booleano de controle, ativação por spacebar (Ver @Eventos)
+    #   jump_count: Variável base responsável pelo movimento, escala com o tempo em que spacebar está pressionado
+    #   MAX_JUMP: Constante que delimita o teto de jump_count, maior distância coberta em um único salto
+    # -------------------------------------------------------------------------------------------------------------
+    if carregando_pulo and jump_count <= MAX_JUMP:
+        jump_count += 1
+
+    plataforma_atual = player.check_underneath(plats)
 
     player.atualizar_pulo()
 
